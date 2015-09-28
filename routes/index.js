ce83c5a015b2e69577f272ffef1594273f7659ab
ales82 <forreg.82@gmail.com>
2015-09-28 13:01:39 +0300

    Adds generic and elements pages and links correct to them

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Prof System - ferestre pentru o viata !' });
 });
 
+/* GET home index page. */
+router.get('/index.html', function(req, res, next) {
+  res.render('index', { title: 'Prof System - ferestre pentru o viata !' });
+});
+
+/* GET generic page. */
+router.get('/generic.html', function(req, res, next) {
+  res.render('generic', { title: 'Generic !' });
+});
+
+/* GET elements page. */
+router.get('/elements.html', function(req, res, next) {
+  res.render('elements', { title: 'Elements!' });
+});
 module.exports = router;
